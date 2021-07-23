02bb15ab371ed2b7ebe6f9053fed5ca85eacf9cf
ben@thedesigncollaborative.co <ben@thedesigncollaborative.co>
2021-07-23 17:10:07 +0100

    Add test for suicide

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,50 @@ mod tests {
     use crate::game::{Board, Stone, State, orthogonally_adjacent_states, Move, Outcome, Illegal};
     use std::collections::HashMap;
     use crate::game::Illegal::{OutOfBounds};
-    use crate::game::Rule::RepeatMove;
+    use crate::game::Rule::{RepeatMove, Suicide};
+
+    #[test]
+    fn cannot_make_suicidal_move() {
+        let mut b = Board {
+            board_states: HashMap::new(),
+            size: 9,
+        };
+
+        let m = Move {
+            intersection: (3, 2),
+            stone: Stone::White,
+        };
+
+        b.update(m);
+
+        let m = Move {
+            intersection: (4, 3),
+            stone: Stone::White,
+        };
+
+        b.update(m);
+
+        let m = Move {
+            intersection: (3, 4),
+            stone: Stone::White,
+        };
+
+        b.update(m);
+
+        let m = Move {
+            intersection: (2, 3),
+            stone: Stone::White,
+        };
+
+        b.update(m);
+
+        let m = Move {
+            intersection: (3, 3),
+            stone: Stone::Black,
+        };
+
+        assert_eq!(b.update(m), Outcome::Illegal(Illegal::Rule(Suicide)))
+    }
 
     #[test]
     fn cannot_make_repeat_move() {
