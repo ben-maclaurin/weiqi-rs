f1cb2bddf0098cb09d029185a3ad8497283f2a4b
ben@thedesigncollaborative.co <ben@thedesigncollaborative.co>
2021-08-10 22:27:01 +0100

    Changes

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
 use warp::Filter;
 use serde::{Serialize, Deserialize};
 use engine::game::{Board, BoardSize};
+use std::collections::HashMap;
+use engine::chain::Chain;
 
 #[derive(Deserialize, Serialize)]
 struct CreateBoard {
@@ -10,17 +12,16 @@ struct CreateBoard {
 #[tokio::main]
 async fn main() {
     let create_board = warp::post()
-    .and(warp::path("board"))
-    .and(warp::path("create"))
+    .and(warp::path("new"))
+    .and(warp::path("game"))
     .and(warp::body::json())
     .map(|board: CreateBoard| {
         if board.size > 0 {
-            return format!("Board created");
+            return format!("New game created");
         } else {
-            return format!("Board not created");
+            return format!("Game failed to create");
         }
     });
-
 
     warp::serve(create_board)
     .run(([127, 0, 0, 1], 8080))
