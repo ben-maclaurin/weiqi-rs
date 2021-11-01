5a3236495a69fd45da3df4a7ef818ec1368894e8
ben@thedesigncollaborative.co <ben@thedesigncollaborative.co>
2021-11-01 18:51:53 +0000

    Update again

--- engine/src/chain.rs
+++ engine/src/chain.rs
@@ -9,16 +9,12 @@ pub struct Chain<'a> {
 
 impl<'a> Chain<'a> {
     pub fn move_is_connected(&self, mov: &Move, board: &Board) -> bool {
-
         // Loop through all chain members.
         for m in &self.moves {
-
             // Get adjacencies of move's intersection.
             for (state, intersection) in adjacencies(&m.intersection, &board) {
-
                 // Check if adjacent state is a stone.
                 if let Some(State::Stone(_)) = state {
-
                     // Return true only if move's stone and intersection match that of
                     // adjacent intersection.
                     if m.stone == mov.stone && intersection == mov.intersection {
@@ -32,8 +28,8 @@ impl<'a> Chain<'a> {
 
     pub fn has_liberties(&self, board: &Board) -> bool {
         for m in &self.moves {
-            for a in adjacencies(&m.intersection, &board) {
-                if let Some(State::Vacant) = a.0 {
+            for (state, _) in adjacencies(&m.intersection, &board) {
+                if let Some(State::Vacant) = state {
                     return true;
                 }
             }
